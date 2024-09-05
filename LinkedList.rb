904cc7caddb299cebd80645cef824329308b4fc1
Yoni190 <yfisuhu@gmail.com>
2024-09-05 17:32:55 +0300

    Create a method to prepend a node

--- LinkedList.rb
+++ LinkedList.rb
@@ -8,12 +8,32 @@ class LinkedList
 
   def append(value)
     if self.head.nil?
-      head = Node.new(value)
+      self.head = Node.new(value)
     else
-      current = head
-      while current.next_node != nil do current = current.next_node end
+      temp = head
+      while temp.next_node != nil do 
+        temp = temp.next_node 
+      end
       current = Node.new(value)
+      temp.next_node = current
+      current.next_node = nil
+    end
+  end
+
+  def prepend(value)
+    
+    if self.head.nil?
+      self.head = Node.new(value)
+      self.head.next_node = nil
+    else
+      current = Node.new(value)
+      current.next_node = head
+      self.head = current
     end
   end
 
 end
+li = LinkedList.new
+li.prepend(4)
+li.append(5)
+li.append(6)
